b37005faf0d1ff4765045fd8b272ea16cc1f57c1
ReplacedSpace17 <replacedspace17@gmail.com>
2025-10-28 19:27:04 -0600

    Example

--- src/config/backend.js
+++ src/config/backend.js
@@ -2,6 +2,6 @@
 
 // Para desarrollo: siempre localhost
 // Para producción: la URL se inyecta durante el build
-const backend = "http://localhost:8080"; // Este será reemplazado en el build
-//const backend = "https://backend-evaluasaurio.singularitymx.org"; // Este será reemplazado en el build
+//const backend = "http://localhost:8080"; // Este será reemplazado en el build
+const backend = "https://backend-evaluasaurio.singularitymx.org"; // Este será reemplazado en el build
 export default backend;
