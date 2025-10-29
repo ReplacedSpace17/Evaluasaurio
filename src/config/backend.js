94635d2c27bf91516734d2d0827561050b2b9953
ReplacedSpace17 <replacedspace17@gmail.com>
2025-10-28 20:27:59 -0600

    Test

--- src/config/backend.js
+++ src/config/backend.js
@@ -1,7 +1,2 @@
-// config/backend.js
-
-// Para desarrollo: siempre localhost
-// Para producción: la URL se inyecta durante el build
-//const backend = "http://localhost:8080"; // Este será reemplazado en el build
-const backend = "https://backend-evaluasaurio.singularitymx.org"; // Este será reemplazado en el build
+const backend = import.meta.env.VITE_BACKEND_URL;
 export default backend;
